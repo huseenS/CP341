eaf20b642aae62f793ab5e97e688e6736b9d1002
huseenS <huseensufi@gmail.com>
2017-09-26 15:53:52 -0600

    forgot to commit

---
+++ theory/startup/ProgrammingWarmUp/src/loop.c
@@ -0,0 +1,19 @@
+/*
+ * loop.c
+ *
+ *  Created on: Sep 25, 2017
+ *      Author: Huseen Sufi
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ void loop(){
+	int i = 1;
+	while(i<11) {
+		printf("%i\n",i);
+		i++;
+	}
+}
